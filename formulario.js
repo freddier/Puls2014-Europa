d7ebc7e3d83e6e4ca688d22c3ec5900fe77fdaa9
Pablo Rigazzi <pablo.rigazzi@gmail.com>
2014-01-29 15:11:50 -0500

    Primera clase de JS, con JSON, LocalStorage y mucho mucho amor

--- formulario.js
+++ formulario.js
@@ -1,15 +1,29 @@
-var $form  = $('#formulario'),
-	$titulo = $('#titulo'),
-	$url = $('#link'),
+var $form  		= $('#formulario'),
+	$titulo 	= $('#titulo'),
+	$url 		= $('#link'),
 	$primerPost = $('.item').first(),
-	$lista = $("#contenido");
+	$lista 		= $("#contenido"),
+	ss 			= sessionStorage,
+	ls 			= localStorage;
+
+if (ls.getItem('autosave')) {
+	$titulo.val(ss.getItem('titulo'));
+	$url.val(ss.getItem('url'));
+}
+
+var id = setInterval(function(){
+	ss.setItem('titulo', $titulo.val());
+	ss.setItem('url', $url.val())
+}, 1000);
 
 function mostrarOcultarFormulario(){
 	$form.slideToggle();
-	return false;
+	$lista.slideToggle();
 }
 
-function agregarPost(){
+function agregarPost(e) {
+	e.preventDefault();
+
 	var titulo = $titulo.val(),
 		url = $url.val(),
 		clone = $primerPost.clone();
@@ -18,13 +32,37 @@ function agregarPost(){
 		.text(titulo)
 		.attr('href', url)
 	
-	clone.hide()
+	clone.hide();
+	$lista.prepend(clone);
+	mostrarOcultarFormulario();
+	$titulo.val("");
+	$url.val("");
 
-	$lista.prepend(clone)
+	clone.fadeIn();
+}
 
-	clone.slideDown()
-	return false
+function grabarInformacion(e) {
+	e.preventDefault();
+
+	var titulo 	= $titulo.val(),
+		url 	= $url.val(),
+		ls 		= localStorage,
+		ss		= sessionStorage;
+
+	ls.setItem('titulo', titulo);
+	ls.setItem('url', url);
+
+	ss.setItem('titulo', titulo);
+	ss.setItem('url', url);
+
+	mostrarOcultarFormulario();
+	$titulo.val("");
+	$url.val("");
 }
 
 $('#publicar_nav a').click( mostrarOcultarFormulario );
-$('#formulario').on('submit', agregarPost)
+$('#formulario').on('submit', agregarPost);
+
+
+
+
