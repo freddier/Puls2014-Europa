d0dd7a86ebf3f99aa4041b002f368465b84ccbd6
Pablo Rigazzi <pablo.rigazzi@gmail.com>
2014-01-30 14:59:58 -0500

    Ejemplo final de Carga con Ajax con ejemplos HTML y Json

--- ajax.js
+++ ajax.js
@@ -18,5 +18,60 @@ function obtenerGeoInformacion(lat, lon) {
 }
 
 function geocallback(datos) {
-	console.log(datos);
+	var info   = datos.query.results.Result;
+	var pais   = info.country;
+	var ciudad = info.city;
+	var barrio = info.neighborhood;
+	var woeid  = info.woeid;
+
+	var tmp = '<p><strong>'+barrio+'</strong><br>'+ciudad+', '+pais+' ('+woeid+')</p>';
+	$('#geo').prepend(tmp);
+
+	obtenerClima(woeid);
 }
+
+function obtenerClima(woeid) {
+	var query = "SELECT * FROM weather.forecast WHERE woeid='"+woeid+"'";
+	query += " AND u='c'";
+	query = encodeURIComponent(query);
+
+	var opciones = {
+		url: base_url + "q=" + query,
+		dataType: 'jsonp',
+		jsonpCallback: 'climacallback',
+		data: {
+			format : 'json'
+		}
+	}
+
+	$.ajax(opciones);
+}
+
+function climacallback(datos) {
+	var clima = datos.query.results.channel;
+	var temp  = clima.item.condition.temp;
+	var code  = clima.item.condition.code;
+	var unit  = clima.units.temperature;
+	var img   = new Image();
+	img.src   = "http://l.yimg.com/a/i/us/we/52/"+code+".gif";
+
+	$('#clima')
+		.html('<strong>'+temp+'</strong> '+unit+'º')
+		.prepend(img);
+}
+
+function loadLogos() {
+	$.get('ajax_html.html', function(html) {
+		$('footer').append(html);
+	});
+}
+
+function loadUser() {
+	$.get('ajax_json.json', function(datos) {
+		var avatar = new Image();
+		avatar.src = datos.avatar;
+		avatar.title = datos.nombre+" "+datos.apellido;
+		$('#avatar').append(avatar);
+	});
+}
+
